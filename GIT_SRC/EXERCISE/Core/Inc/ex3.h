be307825a93b371071e3ceb09bbc0c06b9cb34c7
NAMNGUYEN\NAM NGUYEN <nam.nguyen1308hcmut@hcmut.edu.vn>
2024-10-03 13:59:26 +0700

    no message

---
+++ GIT_SRC/EXERCISE/Core/Inc/ex3.h
@@ -0,0 +1,16 @@
+/*
+ * ex3.h
+ *
+ *  Created on: Sep 28, 2024
+ *      Author: NAM NGUYEN
+ */
+
+#ifndef INC_EX3_H_
+#define INC_EX3_H_
+#include "software_timer.h"
+#include "stdbool.h"
+#include "stdio.h"
+#include "string.h"
+void display7SEG(int num);
+void update7SEG(int index);
+#endif /* INC_EX3_H_ */
